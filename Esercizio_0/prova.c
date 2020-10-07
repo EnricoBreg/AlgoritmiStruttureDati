16843186fe6fe7c4a2dc33560c978a7cff013f71
Enrico <bregolienrico@gmail.com>
2020-10-07 10:15:05 +0200

    Funzionante

---
+++ Esercizio_0/prova.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void InsertionSort(int *A, int low, int high);
+
+
+int main(int argc, char **argv) {
+
+    int *A;
+    int i, j;
+
+
+    return 0;
+}

8de4d45b2fdd749bf867b92ecc80af203c6759a0
Enrico Bregoli <enrico.bregoli@edu.unife.it>
2020-10-29 10:33:53 +0100

    Problema con MergeSort sistemato, pseudocodice di wikipedia

--- Esercizio_1/provaMergeSort.c
+++ Esercizio_1/provaMergeSort.c
@@ -1,113 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DIM 12
+#define DIM 8
 #define SEED 18
-//#define RANDOM
+#define MAXRAND 100
+// se definito RANDOM il programma genera un array di dimensione DIM con elementi casuali compresi tra 1 e MAXRAND
+#define RANDOM
 
-void merge(int*, int, int, int );
-void mergeSort(int*, int, int);
-void stampaArray(int A[]);
+void merge(int *a, int left, int center, int right);
+void mergeSort(int *a, int left, int right);
+void stampaArray(int a[]);
 
 
 /* MAIN FUNCTION
  * Prova implementazione C dell'algoritmo di ordinamento MergeSort
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     int i;
     
     #ifdef RANDOM
-    int *A;
-    A = (int*)malloc(sizeof(int) * DIM);
+    int *a;
+    a = (int*)malloc(sizeof(int) * DIM);
     // Inizializzazione dell'array con numero pseudo-casuali
     for(i = 0; i < DIM; i++) {
-        A[i] = 1 + rand() % 100;
+        a[i] = 1 + rand() % MAXRAND;
     }
     #else
-    int A[] = {34, 77, 29, 96, 28, 10, 16, 9, 69, 39, 98, 23};
+    int a[] = {10, 3, 15, 2, 1, 4, 9, 0};
     #endif
     
-    stampaArray(A);
-    mergeSort(A, 0, DIM-1);
+    stampaArray(a);
 
-    stampaArray(A);
+    mergeSort(a, 0, DIM-1);
+
+    stampaArray(a);
 
     #ifdef RANDOM
     // deallocazione della memoria riservata all'array A
-    free(A);
+    free(a);
     #endif
 
     return 0;
 }
 
-void merge(int *A, int p, int q, int r) {
-    int n1 = q - p;
-    int n2 = r - q;
-    int i, j, k;
-    // Array di appoggio
-    int *L, *R;
-    L = (int*)malloc(n1 * sizeof(int));
-    R = (int*)malloc(n2 * sizeof(int));
-
-    /* 
-     * Gli elementi dall'indice (p+i-1) a n1 vengono copiati nell'array di appoggio L
-     * Gli elementi dall'indice q a n2 vengono copiati nell'array di appoggio R
-     */
-    for(i = 0; i < n1; i++) {    
-        L[i] = A[p + i];   
+void mergeSort(int a[], int left, int right) 
+{
+    int center;
+    if(left < right) {
+        center = (left + right) / 2;
+        mergeSort(a, left, center);
+        mergeSort(a, center+1, right);
+        merge(a, left, center, right);
     }
+    return;
+}
 
-    for(j = 0; j < n2; j++) {
-        R[j] = A[q + j];
-    }
+void merge(int *a, int left, int center, int right) 
+{
+    int i, j, k, *b;
+    i = left;
+    j = center + 1;
+    k = 0;
+    // L'array b ha dimensione right-left+1, cioè la dimensione dell'array a passato
+    b = (int*)malloc(sizeof(int) * (right-left+1));
 
-    i = j = 0;
-
-    for(k = p; k < r; k++) {
-        if(i < n1) {
-            if(L[i] <= R[j] || j == n2 + 1) {
-                // Copia da L ed incrementa i
-                A[k] = L[i];
-                i++;
-            }
-            else 
-            {
-                // Copia da R ed incrementa j
-                A[k] = R[j];
-                j++;
-            }
+    while((i <= center) && (j <= right)) 
+    {
+        if(a[i] <= a[j])
+        {
+            b[k] = a[i];
+            i++;
         }
-        else {
-            // Copia da R ed incrementa j
-            A[k] = R[j];
+        else
+        {
+            b[k] = a[j];
             j++;
         }
+        k++;
     }
 
-    free(L);
-    free(R);
+    // Rimangono da copiare gli eventuali elementi ancora da copiare
+    while(i <= center) 
+    {
+        b[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while(j <= right) 
+    {
+        b[k] = a[j];
+        j++;
+        k++;
+    }
+
+    // ricopiatura degli elementi dall'array di appoggio b all'array di appoggio a
+    for(k = left; k <= right; k++) {
+        a[k] = b[k - left];
+    }
+
+    // deallocazione della memoria allocata nella heap per l'array b
+    free(b);
 
     return;
 }
 
-void mergeSort(int* A, int p, int r) {
-    int q;
-
-    if(p < r) {
-        q = (p+r) / 2;
-        mergeSort(A, p, q);
-        mergeSort(A, q+1, r);
-        merge(A, p, q, r);
-    }
-    
-    return;
-}
 
 
-void stampaArray(int A[]) {
+void stampaArray(int a[]) 
+{
     int i;
     for(i = 0; i < DIM; i++) {
-        printf("%d | ", A[i]);
+        printf("%d | ", a[i]);
     }
     printf("\n");
     return;

ca7dc001df9223a95b4898c0e710400bed45efc8
Enrico Bregoli <enrico.bregoli@edu.unife.it>
2020-12-24 11:53:35 +0100

    _

--- PROVE/mergeSort.c
+++ PROVE/mergeSort.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-    
+const int dim = 10;
 
+
+void generateRandomArray(int *a, const int dim);
+void merge(int *a, int p, int r);
+void mergeSort(int *a, int p, int q, int r);
+
+int main(int argc, char const *argv[]) {
+    
+    
 
 
 
     return 0;
 }
 
+void generateRandomArray(int *a, const int dim) {
+    for(unsigned int i = 0; i < dim; i++) {
+        a[i] = rand() % RAND_MAX + 1;
+    }
+}

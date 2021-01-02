350a8eeedc640e591974073bc66589562ee05c35
Enrico Bregoli <enrico.bregoli@edu.unife.it>
2021-01-02 12:56:04 +0100

    Prototipi di funzione

--- Esercizio_2/my-list-hashtbl-functions.h
+++ Esercizio_2/my-list-hashtbl-functions.h
@@ -46,3 +46,114 @@ typedef struct hashtable_t
 } hashtable_t;
 
 // ----- End of HASHTABLE ----- //
+
+/*--------------- DICHIARAZIONE DELLE FUNZIONI ---------------*/
+
+// ----- LINKED LIST ----- //
+/**
+ * @brief Create a new linked list node.
+ * @param Value that the linked list node should contain.
+ * @return Created linked list node.
+ */
+linkedListNode_t* createLinkedListNode(const int);
+
+/**
+ * @brief Create a new linked list.
+ * @return Created linked list.
+ */
+linkedList_t* createLinkedList(void);
+
+/**
+ * @brief Insert linked list node in the head of the linked list.
+ * @param The linked list.
+ * @param Linked list node to be inserted.
+ */
+void linkedListInsert(linkedList_t*, linkedListNode_t*);
+
+/**
+ * @brief Search for a value in the linked list.
+ * @param The linked list.
+ * @param Value to be searched for.
+ * @return First linked list node containing such value, if it exists; otherwise, NULL.
+*/
+linkedListNode_t* linkedListSearch(linkedList_t*, const int);
+
+/**
+ * @brief Delete a linked list node from linked list.
+ * @param The linked list.
+ * @param The linked list node to be deleted.
+ */
+void linkedListDelete(linkedList_t*, linkedListNode_t*);
+
+/**
+ * @brief Print the linked list.
+ * @param Linked list to be printed.
+ */
+void linkedListPrint(linkedList_t*);
+
+/**
+ * @brief Free the linked list.
+ * @param Linked list to be freed.
+ */
+void linkedListFree(linkedList_t*);
+
+// ----- End of LINKED LIST ----- //
+
+// ----- HASHTABLE ----- //
+
+/**
+ * @brief Create a new hashtable.
+ * @param The size of the hashtable (i.e., the number of entries).
+ * @return The created hashtable.
+ */
+hashtable_t* createHashtable(const unsigned int);
+
+/**
+ * @brief Hash function computing the key for a given integer.
+ * @param The hashtable needed to access the size of it.
+ * @param The integer for which the key must be computed.
+ * @return The computed key.
+ */
+const unsigned int hashFunction(hashtable_t*, const int);
+
+/**
+ * @brief Insert value in the hashtable.
+ * @param The hashtable.
+ * @param Value to be inserted.
+ */
+void hashtableInsert(hashtable_t*, const int);
+
+/**
+ * @brief Search for a value in the hashtable.
+ * @param The hashtable.
+ * @param Value to be searched.
+ * @return Linked list node containing such value, if it exists; otherwise, NULL.
+ */
+linkedListNode_t* hashtableSearch(hashtable_t*, const int);
+
+/**
+ * @brief Delete value from hashtable.
+ * @param The hashtable.
+ * @param Linked list node to be deleted.
+ */
+void hashtableDelete(hashtable_t*, linkedListNode_t*);
+
+/**
+ * @brief Print the hashtable.
+ * @param Hashtable to be printed.
+ */
+void hashtablePrint(hashtable_t*);
+
+/**
+ * @brief Free hashtable.
+ * @param Hashtable to be freed.
+ */
+void hashtableFree(hashtable_t*);
+
+/**
+ * @brief Test hashtable implementation.
+ * @return True if it is correct; otherwise, false.
+ */
+bool hashtableTest();
+
+// ----- End of HASHTABLE ----- //
